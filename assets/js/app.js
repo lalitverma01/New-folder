49d9d3084546840fbe59630f74139c9091c2657a
lalitverma01 <lalitverma2923@gmail.comm>
2025-03-27 11:34:34 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+const navbarbtn = () => {
+    document.getElementById("name").classList.toggle("show-navbar")
+    document.getElementById("rotates").classList.toggle("rotate-42")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
+    document.getElementById("rotates-2").classList.toggle("rotate-43")
+    document.body.classList.toggle("overflow-hidden")
+}
+
+
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
